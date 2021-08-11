49767dd07f281c5d3d415df3c3c38ce0e4412193
jonathan borbor <jonathanbor2020@gmail.com>
2021-08-11 16:19:27 -0500

    cds

--- menu-GRA-es_EC.js
+++ menu-GRA-es_EC.js
@@ -2,11 +2,11 @@
     "COMMONS": {
         "MENU": {
             "MNU_GRA_PADR":"Menu Padre GrupoA",
-			"MNU_KOU":"Formulario 1",
-			"MNU_EST2":"Formulario 2",
-			"MNU_TONY_GRA":"Formulario 3",
-			"MNU_RSZR_GRA":"Formulario 4",
-			"MNU_JSH_GRA":"Formulario 5"
+			"MNU_KOU":"Formulario 1 - Kevin Ulquilango",
+			"MNU_EST2":"Formulario 2 - Jonathan Borbor",
+			"MNU_TONY_GRA":"Formulario 3 - Jhon Balon",
+			"MNU_RSZR_GRA":"Formulario 4 - Rody Zamora",
+			"MNU_JSH_GRA":"Formulario 5 - Jose Santisteban"
         }
     }
 }
