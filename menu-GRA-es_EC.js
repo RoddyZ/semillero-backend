db2cf3aa4714d1a6a0c2fbb9d16c93bdbe6c6c42
Kevin Ulquiango <78171886+KevinUlquiango@users.noreply.github.com>
2021-08-11 11:44:57 -0500

    url estudiante2

--- menu-GRA-es_EC.js
+++ menu-GRA-es_EC.js
@@ -3,7 +3,7 @@
         "MENU": {
             "MNU_GRA_PADR":"Menu Padre GrupoA",
 			"FORM_KOU":"Formulario 1",
-			"FORM_KOU":"Formulario 2",
+			"MNU_EST2":"Formulario 2",
 			"MNU_TONY_GRA":"Formulario 3",
 			"MNU_RSZR_GRA":"Formulario 4",
 			"MNU_JSH_GRA":"Formulario 5"
