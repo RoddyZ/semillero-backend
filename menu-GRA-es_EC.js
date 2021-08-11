7e937139a055ad94fcd5895a21e0675b6bae264c
RoddyZ <roddy.zamora@epn.edu.ec>
2021-08-11 11:34:54 -0500

    Se agrego los menus traducidos

---
+++ menu-GRA-es_EC.js
@@ -0,0 +1,12 @@
+{
+    "COMMONS": {
+        "MENU": {
+            "MNU_GRA_PADR":"Menu Padre GrupoA",
+			"FORM_KOU":"Formulario 1",
+			"FORM_KOU":"Formulario 2",
+			"MNU_TONY_GRA":"Formulario 3",
+			"MNU_RSZR_GRA":"Formulario 4",
+			"MNU_JSH_GRA":"Formulario 5"
+        }
+    }
+}
